315fbb8c4d51e49cc50f3240ec3214b5f766aea2
Ann <anmatyushkova@gmail.com>
2025-05-20 19:41:03 +0300

    second part error edt

--- script.js
+++ script.js
@@ -18,22 +18,20 @@ let appData = {
 
 
 for (let i = 0; i < 2; i++) {
-    let a1 = prompt("Введите обязательную статью расходов в этом месяце", '');
-	let a2 = prompt("Во сколько обойдется?", '');
+    let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+	let b = prompt("Во сколько обойдется?", '');
 
          if ( (typeof(a)) === 'string' && (typeof(a) != null) && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
             console.log('done')
-        appData.exprenses[a] = b;
-         } else {
-
-         }
+        appData.expenses[a] = b;
+         } 
 
         
-}
+};
 
-appData.moneyPerDay = appData.budget / 30;
+appData.moneyPerDay = appData.budget / 30; 
 
-alert("Daily expences: " + appDataData.moneyPerDay);
+alert("Daily expences: " + appData.moneyPerDay);
 
 if (appData.money < 100) {
     console.log('low income');
